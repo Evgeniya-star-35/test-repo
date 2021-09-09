21c0c322221e6ab416101ceca5b38530bd2e4b3f
petroDavydov <78567740+petroDavydov@users.noreply.github.com>
2021-09-09 14:07:12 +0000

    Deploying to gh-pages from @ Evgeniya-star-35/test-repo@0209a5b707b45c20a10da4674485cccf3297b621 🚀

--- src.cb95bad5.js
+++ src.cb95bad5.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/team.jpg":[["team.9d5348c1.jpg","TFb7"],"TFb7"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-repo/src.d3f2a826.js.map
+//# sourceMappingURL=/test-repo/src.cb95bad5.js.map
