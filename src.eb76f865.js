75bd4efe956396b8245a2036293217fa106af025
Kovganna <annakovalenko168@gmail.com>
2021-09-09 16:42:28 +0000

    Deploying to gh-pages from @ Evgeniya-star-35/test-repo@4073d413ed16160309beddaffd50cf401f2be168 🚀

--- src.eb76f865.js
+++ src.eb76f865.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/team.jpg":[["team.9d5348c1.jpg","TFb7"],"TFb7"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-repo/src.cb95bad5.js.map
+//# sourceMappingURL=/test-repo/src.eb76f865.js.map
