ff73b688f531a65f928adfe83334d4cb83cfc91f
Evgeniya-star-35 <78686547+Evgeniya-star-35@users.noreply.github.com>
2021-09-09 10:25:49 +0000

    Deploying to gh-pages from @ Evgeniya-star-35/test-repo@fde4cf92e2ad1434f72de3e051f8a9eeb8f8b2f4 🚀

--- src.4ddacbb0.js
+++ src.4ddacbb0.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-repo/src.793a851a.js.map
+//# sourceMappingURL=/test-repo/src.4ddacbb0.js.map
