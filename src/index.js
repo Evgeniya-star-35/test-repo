e7264890b11935265a1eeb506ab00a06b43d9bd7
marta <Vynnytska1989@gmail.com>
2021-09-10 10:14:26 +0300

    наша команда

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './sass/main.scss';
+
+import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+// Initialise Carousel
+const newsCarousel = new Carousel(document.querySelector("#newsCarousel"), {
+    infinite: false,
+    Dots: false,
+    friction: 0.89,
+    slidesToSlide: 3,
+    fill: false,
+  });
